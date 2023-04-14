4fe8df14334cecd18ce6925a9cbe45906f94826a
Sambasivarao.Edara@ge.com <Sambasivarao.Edara@ge.com>
2023-04-14 16:39:14 -0700

    Intial questions

---
+++ Questions/1_MadLibGenerator.py
@@ -0,0 +1,3 @@
+This is one of the most fun beginner Python projects, not to mention it lets you practice how to use strings, variables, and concatenation.
+
+The Mad Libs Generator gathers and manipulates user input data as an adjective, a pronoun, and a verb. The program takes this data and arranges it to build a story.
---
+++ Questions/5_HangmanGame.py
@@ -0,0 +1,5 @@
+This is an enjoyable Python project idea to emulate the word-guessing game, Hangman. We’ve used a predefined list of words for the guessing aspect, but feel free to improve this by using a third-party dictionary API.
+
+This Python project uses loops, functions, and string formatting to print the hangman’s progress. It also lets you experiment with the standard library's random, time, and os modules.
+
+Specifically, we’ve used the random module to select the word to guess, the os module to clear the screen, and the time module’s .sleep() function to introduce pauses to enhance the game flow.
